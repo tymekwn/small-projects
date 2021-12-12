2aa9fa272599e939425499e6163e3a2d3d831427
Tymoteusz Niewodniczanski <tymekwn@gmail.com>
2021-12-12 18:27:45 +0000

    Cleaning again!

--- quickfibo.py
+++ quickfibo.py
@@ -1,4 +1,4 @@
 fibolist = []
 term = int(input("Enter the number you're looking for"))
 for i in range(term): fibolist.append(fibolist[-1]+fibolist[-2]) if i >= 2 else fibolist.append(1)
-for i,num in enumerate(fibolist): print(i+1,num) if i+1 == term else print(i)
+for i,num in enumerate(fibolist): print(i+1,num) if i+1 == term else print(i+1)
